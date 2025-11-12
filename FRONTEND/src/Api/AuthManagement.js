48219b05225a0470a3a9a42b1f83cb57f548a54c
dev-subha-dot <subhashinimca57@gmail.com>
2025-11-12 17:02:00 +0530

    small_changes_check

--- FRONTEND/src/Api/AuthManagement.js
+++ FRONTEND/src/Api/AuthManagement.js
@@ -7,7 +7,7 @@ export const loginUser = async (formData) => {
     return axios.post(`${baseUrl}login`, formData);
 };
 
-// Register APIs
+// Register API
 export const registerUser = async (formData) => {
     return axios.post(`${baseUrl}register`, formData);
 };
